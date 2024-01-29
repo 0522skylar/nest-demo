a04c9d63e10dd54f22946f8b6adbdf512bbd9b84
huanghaiyan <huanghaiyan@hltn.com>
2024-01-29 18:05:29 +0800

    feat: add rxjs

---
+++ serive/test.js
@@ -0,0 +1,11 @@
+import { of, filter, map } from "rxjs";
+
+// RxJS 是一个组织异步逻辑的库，它有很多 operator，可以极大的简化异步逻辑的编写。
+
+// 它是由数据源产生数据，经过一系列 operator 的处理，最后传给接收者。
+
+// 这个数据源叫做 observable。
+of(1, 2, 3)
+  .pipe(map((x) => x * x))
+  .pipe(filter((x) => x % 2 !== 0))
+  .subscribe((v) => console.log(`value: ${v}`));
